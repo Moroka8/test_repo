804e8a0b3ae29eeef3581856149b18d35e2efc3a
Moroka8 <moroka8@mail.ru>
2022-11-12 22:33:48 +0700

    add prod

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,14 +3,20 @@ int a = new int();
 int b = new int();
 Console.WriteLine("Type:\n\ta - insert value of a\n\tb - insert value of b\n\t+ - perform summation\n\t- - perform substraction\n\t* - perform multiplication\n\t/ - perform division");
 string command = Console.ReadLine();
-if (command != null) {
+if (command != null)
+{
     if (command == "a")
     {
         Console.WriteLine("Set value:\n");
         a = Int32.Parse(Console.ReadLine());
     }
-    else if (command == "b") {
+    else if (command == "b")
+    {
         Console.WriteLine("Set value:\n");
         b = Int32.Parse(Console.ReadLine());
+    }
+    else if (command == "*")
+    {
+        Console.WriteLine(a * b);
     }
 }
