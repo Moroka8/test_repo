75b520181234b5a2d3421e9061c245814e2634fd
Moroka8 <moroka8@mail.ru>
2022-11-12 22:47:34 +0700

    microchange

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,9 +2,9 @@
 int a = new int();
 int b = new int();
 Console.WriteLine("Type:\n\ta - insert value of a\n\tb - insert value of b\n\t+ - perform summation\n\t- - perform substraction\n\t* - perform multiplication\n\t/ - perform division");
-string command = Console.ReadLine();
 while (true)
 {
+    string command = Console.ReadLine();
     if (command != null)
     {
         if (command == "a")
